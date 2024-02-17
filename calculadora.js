d85de703d791f3e7ca208b317d576616b75ce1ca
Gabrielaritadev <gabrielarita1995@gmail.com>
2024-02-17 15:59:51 -0300

    projeto calculadora

---
+++ calculadora.js
@@ -0,0 +1,30 @@
+function calcular(vitorias, derrotas){
+    let resultado = 200-5 ;
+    return resultado
+}
+let resultado = calcular(200-5)
+
+if (resultado < 10){git
+    
+    console.log ("Voce esta no nivel ferro ")
+}
+else if (resultado > 10 && resultado <=20){ 
+    console.log ("Voce esta no nivel Bronze")
+}
+else if (resultado = 21  && resultado <=50){ 
+    console.log ("Voce está no nivel Prata")
+}
+else if (resultado = 51  && resultado <=80){
+    console.log ("Voce está no nivel Ouro")
+}
+else if (resultado = 81  && resultado <=90){ 
+    console.log ("Voce está no nivel Diamante")
+}
+else if (resultado = 91  && resultado <=100){ 
+    console.log ("Voce está no nivel Lendário")
+}
+else if (resultado >= 101 ){ 
+    console.log ("Voce está no nivel Imortal")
+}
+
+console.log ("O Herói tem de saldo um total de " + resultado + " e está no nivel" )
